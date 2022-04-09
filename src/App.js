0bdfac93f6820ffcc29470aa26052f36419b7e91
ganasa18 <ganasa18@yahoo.com>
2022-04-09 17:45:24 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import {NavigationContainer} from '@react-navigation/native';
+import React from 'react';
+import 'react-native-gesture-handler';
+import {SplashScreen} from './pages';
+
+const App = () => {
+  return (
+    <>
+      <NavigationContainer>
+        <SplashScreen />
+      </NavigationContainer>
+    </>
+  );
+};
+
+export default App;
---
+++ src/pages/SplashScreen/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+import {Logo} from '../../assets';
+
+const SplashScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Logo />
+      <View style={{height: 38}} />
+      <Text style={styles.text}>Food Market</Text>
+    </View>
+  );
+};
+
+export default SplashScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#FFC700',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  text: {
+    color: '#020202',
+    fontSize: 32,
+    fontFamily: 'Poppins-Medium',
+  },
+});
